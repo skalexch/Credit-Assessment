1816d64553419e742abc2459b334f2cbde8d72e9
DELL <saifbn@gmail.com>
2020-05-21 00:11:33 +0100

    added form

--- src/app/public-interface/public-interface.component.ts
+++ src/app/public-interface/public-interface.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import {Credit} from 'src/app/_models'
+import { FormGroup, FormControl, Validators, FormBuilder } 
+    from '@angular/forms';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-public-interface',
   templateUrl: './public-interface.component.html',
   styleUrls: ['./public-interface.component.css']
 })
-export class PublicInterfaceComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class PublicInterfaceComponent  {
+  form: FormGroup;
+    
+  firstName = new FormControl("", Validators.required);
+  lastName = new FormControl("", Validators.required);
+  cin=new FormControl("", Validators.required);
+  email=new FormControl("", Validators.required);
+  situation=new FormControl("", Validators.required);
+  kids=new FormControl("", Validators.required);
+  birthday=new FormControl("", Validators.required);
+  oldCredit=new FormControl("", Validators.required);
+  annuity=new FormControl("", Validators.required);
+  creditAmount=new FormControl("", Validators.required);
+  creditPeriod=new FormControl("", Validators.required);
+  
+  constructor(fb: FormBuilder) {
+      this.form = fb.group({
+          "firstName": this.firstName,
+          "lastName": this.lastName,
+          "cin": this.cin,
+          "email": this.email,
+          "situation": this.situation,
+          "kids": this.kids,
+          "birthday": this.birthday,
+          "oldCredit": this.oldCredit,
+          "annuity": this.annuity,
+          "creditAmount": this.creditAmount,
+          "creditPeriod": this.creditPeriod
+      });
   }
+  onSubmitModelBased() {
+      console.log("model-based form submitted");
+      console.log(this.form);
+      console.warn(this.form.value);
 
+  }
 }
